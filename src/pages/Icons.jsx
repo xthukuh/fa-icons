8c477ea1f22ac4af8e88f6a13424caecca080fdc
Thuku <xthukuh@gmail.com>
2022-05-29 10:09:55 +0300

    resposive fix

--- src/pages/Icons.jsx
+++ src/pages/Icons.jsx
@@ -80,7 +80,7 @@ const Icons = () => {
 	const searchIcons = React.useMemo(() => {
 		const searchQuery = search.trim();
 		return !searchQuery ? typeIcons : typeIcons.filter(item => {
-			return item.iconName.indexOf(searchQuery) !== -1;
+			return item.iconName.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1;
 		});
 	}, [typeIcons, search]);
 
@@ -88,40 +88,44 @@ const Icons = () => {
 
 	return (
 		<Section>
-			<div className="bg-gray-200 p-2">
+			<div className="bg-gray-200 pt-2">
 				<h1 className="text-3xl text-blue-500">Fa Icons</h1>
 				<p className="text-base text-gray-600 font-bold italic">
 					<span>Fortawesome {IconTypes[iconType]}</span>&nbsp;
 					<span>({`${searchIcons.length === typeIcons.length ? typeIcons.length : searchIcons.length + '/' + typeIcons.length}`} icons)</span>
 				</p>
-				<div className="mt-2 pt-2 flex items-center border-t border-gray-400">
-					{Object.keys(IconTypes).map((key, index) => {
-						return (
-							<label key={index} className="w-20 whitespace-nowrap cursor-pointer">
-								<input
-									type="radio"
-									name="type"
-									value={key}
-									onChange={onIconType}
-									defaultChecked={iconType === key} />
-								<span className="ml-1">{IconTypes[key]}</span>
-							</label>
-						);
-					})}
-					<input
-					type="text"
-					className="px-2 py-1 outline-none border border-gray-500 flex-grow"
-					placeholder="Search"
-					onChange={onSearch}
-					value={search}
-					/>
-					<button type="button" className="ml-2 bg-gray-500 text-white px-2 py-1" onClick={onClear}>Clear</button>
+				<div className="mt-2 flex flex-col md:flex-row border-t border-gray-400">
+					<div className="pt-2 mr-4 flex flex-row items-center">
+						{Object.keys(IconTypes).map((key, index) => {
+							return (
+								<label key={index} className="w-20 whitespace-nowrap cursor-pointer">
+									<input
+										type="radio"
+										name="type"
+										value={key}
+										onChange={onIconType}
+										defaultChecked={iconType === key} />
+									<span className="ml-1">{IconTypes[key]}</span>
+								</label>
+							);
+						})}
+					</div>
+					<div className="pt-2 flex-grow flex flex-row items-center">
+						<input
+						type="text"
+						className="px-2 py-1 outline-none border border-gray-500 flex-grow"
+						placeholder="Search"
+						onChange={onSearch}
+						value={search}
+						/>
+						<button type="button" className="ml-2 bg-gray-500 text-white px-2 py-1" onClick={onClear}>Clear</button>
+					</div>
 				</div>
 			</div>
-			<div className="flex flex-row gap-1">
+			<div className="flex flex-row gap-1 mt-2">
 				{!selected ? <div className="p-3 bg-gray-900 text-white text-xs font-mono flex-grow">Click to select icon.</div> : (
 					<>
-					<div class="flex justify-center items-center w-20 bg-gray-900 text-white">
+					<div class="flex justify-center items-center w-20 bg-gray-900 text-white p-4">
 						<Fa className="text-3xl" icon={[selected.prefix, selected.iconName]} fixedWidth />
 					</div>
 					<div className="p-3 bg-gray-900 text-white text-xs font-mono flex-grow">
@@ -131,16 +135,16 @@ const Icons = () => {
 					</>
 				)}
 			</div>
-			<div className="border-bottom my-3 flex flex-wrap gap-2">
+			<div className="border-bottom my-3 gap-2 grid grid-cols-3 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10">
 				{searchIcons.map((item, i) => {
 					return (
 						<a
 						key={i}
-						className={`w-24 p-2 text-center cursor-pointer ${isSelected(item) ? 'bg-blue-500 hover:bg-blue-700 text-white' : 'bg-white hover:bg-gray-300 text-gray-800'}`}
+						className={`w-full px-2 py-4 text-center cursor-pointer ${isSelected(item) ? 'bg-blue-500 hover:bg-blue-700 text-white' : 'bg-white hover:bg-gray-300 text-gray-800'}`}
 						style={{fontSize: '10px', '&:active': {backgroundColor: 'white'}}}
 						onClick={() => onItem(item)}
 						title={`[${item.prefix}, ${item.iconName}]`}>
-							<Fa className="text-xl" icon={[item.prefix, item.iconName]} fixedWidth />
+							<Fa className="text-2xl" icon={[item.prefix, item.iconName]} fixedWidth />
 							<p className="mt-2 text-xs">
 								{item.iconName.replace(/-/g, ' ')}
 							</p>
